8b0ca0f6e7d938c814a6040cc5ebe32dde8ffbdb
Ricky Wu <rickywu209@gmail.com>
2022-09-25 15:25:00 -0600

    adding map api in our component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,12 @@ import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import { classes } from "istanbul-lib-coverage";
 import SearchIcon from '@material-ui/icons/Search';
 
+import useStyles from './styles';
+
 const Header = () => {
+    const classes = useStyles();
+
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -15,14 +20,14 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <Autocomplete>
+          {/* <Autocomplete> */}
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }}/>
             </div>
-          </Autocomplete>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
