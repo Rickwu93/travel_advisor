482bea9cd8b0af71bcc398e510441488e03c74fd
Ricky Wu <rickywu209@gmail.com>
2022-10-06 01:21:05 -0600

    search functionality and env file added

--- src/App.js
+++ src/App.js
@@ -33,20 +33,22 @@ const App = () => {
     }, [rating]);
 
     useEffect(() => {
+        if(bounds.sw && bounds.ne) {
         setIsLoading(true);
 
         getPlacesData(type, bounds.sw, bounds.ne)
-        .then((data) => {
-            setPlaces(data);
-            setfilteredPlaces([])
-            setIsLoading(false);
-        })
-    }, [type, coordinates, bounds]);
+            .then((data) => {
+                setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                setfilteredPlaces([])
+                setIsLoading(false);
+            })
+        }
+    }, [type, bounds]);
 
     return (
         <>
             <CssBaseline />
-            <Header />
+            <Header setCoordinates={setCoordinates} />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List 
